6ca00ce3bd3d7fe552b82df4e92936ec8d76a36b
AnatolSalau <salovanatoly5@mail.ru>
2023-10-18 16:21:17 +0300

    Added realization QuickSort.java

--- quick_sort/src/QuickSort.java
+++ quick_sort/src/QuickSort.java
@@ -1,43 +1,91 @@
 import java.util.Arrays;
 
 public class QuickSort {
+
       public static void main(String[] args) {
+            //int[] array = new int[]{64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+            int[] array = { 10, 7, 8, 9, 1};
 
-            int[] arr = {10, 7, 8, 9, 1, 5};
-            System.out.println("array before : " + Arrays.toString(arr));
+            System.out.println("array before : " + Arrays.toString(array));
 
-            quickSort(arr, 0, arr.length-1);
+            quickSort(array, 0, array.length - 1);
 
-            System.out.println("array after : " + Arrays.toString(arr));
+            System.out.println("array after : " + Arrays.toString(array));
       }
 
-      public static void quickSort(int arr[], int beginIndex, int endIndex) {
-            if (beginIndex < endIndex) {
-                  int partitionIndex = partition(arr, beginIndex, endIndex);
+      public static void quickSort(int[] arr, int beginIndex, int endIndex) {
+            int beginNumber = arr[beginIndex];
+            int endNumber = arr[endIndex];
 
-                  quickSort(arr, beginIndex, partitionIndex-1);
-                  quickSort(arr, partitionIndex+1, endIndex);
+            System.out.println("quickSort started : " + Arrays.toString(arr) +
+                  ", beginIndex = " + beginIndex +
+                  " (beginNumber " + beginNumber +
+                  "), endIndex = " + endIndex +
+                  " (endNumber " + endNumber +
+                  ")");
+            if (beginIndex < endIndex) {
+                  int divideIndex = partition(arr, beginIndex, endIndex);
+                  int divideNumber = arr[divideIndex];
+                  System.out.println("beginIndex < endIndex, divideIndex = " + divideIndex +
+                  ", (divideNumber " + divideNumber + ")");
+                  quickSort(arr, beginIndex, divideIndex - 1);
+
+                  quickSort(arr, divideIndex, endIndex);
             }
       }
 
-      private static int partition(int arr[], int beginIndex, int endIndex) {
-            int pivot = arr[endIndex];
-            int i = (beginIndex-1);
+      private static int partition(int[] arr, int beginIndex, int endIndex) {
+            int rightIndex = endIndex;
+            int leftIndex = beginIndex;
 
-            for (int j = beginIndex; j < endIndex; j++) {
-                  if (arr[j] <= pivot) {
-                        i++;
+            int pivot = arr[beginIndex + (endIndex - beginIndex) / 2];
 
-                        int swapTemp = arr[i];
-                        arr[i] = arr[j];
-                        arr[j] = swapTemp;
+            int leftNumber = arr[leftIndex];
+            int rightNumber = arr[rightIndex];
+
+            System.out.println("partition started : " + Arrays.toString(arr) +
+                  ", leftIndex = " + leftIndex +
+                  " (leftNumber " + leftNumber +
+                  "), rightIndex = " + rightIndex +
+                  " (rightNumber " + rightNumber +
+                  ")" +
+                  ", pivotNumber = " + pivot);
+
+            while (leftIndex <= rightIndex) {
+
+                  while (arr[leftIndex] < pivot) {
+                        leftIndex++;
+                        System.out.println("leftIndex++, (leftIndex = " + leftIndex + ", leftNumber = " + arr[leftIndex] + ")");
+                  }
+
+                  while (arr[rightIndex] > pivot) {
+                        rightIndex--;
+                        System.out.println("rightIndex--, (rightIndex = " + rightIndex + ", rightNumber = " + arr[rightIndex] + ")");
+                  }
+
+                  if (leftIndex <= rightIndex) {
+                        System.out.println("leftIndex <= rightIndex");
+                        swap(arr, rightIndex, leftIndex);
+                        leftIndex++;
+                        rightIndex--;
+                        System.out.println("leftIndex++, (leftIndex = " + leftIndex + ")");
+                        System.out.println("rightIndex--, (rightIndex = " + rightIndex + ")");
                   }
             }
+            return leftIndex;
+      }
 
-            int swapTemp = arr[i+1];
-            arr[i+1] = arr[endIndex];
-            arr[endIndex] = swapTemp;
+      private static void swap(int[] arr, int index1, int index2) {
 
-            return i+1;
+            int index1Number = arr[index1];
+            int index2Number = arr[index2];
+            System.out.println("swap started : " + Arrays.toString(arr) +
+                  ", index1 = " + index1 +
+                  " (index1Number " + index1Number +
+                  "), index2 = " + index2 +
+                  " (index2Number " + index2Number + ")");
+            int tmp = arr[index1];
+            arr[index1] = arr[index2];
+            arr[index2] = tmp;
       }
 }
