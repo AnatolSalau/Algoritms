77c5ce81e06a0ec2f146f8a75ee93c3bc5a1e1aa
AnatolSalau <salovanatoly5@mail.ru>
2023-10-18 13:43:56 +0300

    Added realization QuickSort.java

--- quick_sort/src/QuickSort.java
+++ quick_sort/src/QuickSort.java
@@ -2,25 +2,42 @@ import java.util.Arrays;
 
 public class QuickSort {
       public static void main(String[] args) {
-            int[] arr = {4, 5, 2, 3};
+
+            int[] arr = {10, 7, 8, 9, 1, 5};
+            System.out.println("array before : " + Arrays.toString(arr));
+
+            quickSort(arr, 0, arr.length-1);
+
+            System.out.println("array after : " + Arrays.toString(arr));
       }
 
-      private static void doSort(int[] arr, int low, int high) {
-            //Выход из рекурсии
-            if (arr.length == 0 || low >= high) return;
+      public static void quickSort(int arr[], int beginIndex, int endIndex) {
+            if (beginIndex < endIndex) {
+                  int partitionIndex = partition(arr, beginIndex, endIndex);
 
-            //Выбираем опорный элемент
-            //Индекс середины
-            int middle = low + (high - low) / 2;
-            //элемент сравнения
-            int border = arr[middle];
-
-            //разделяем на подмассивы, сравниваем и меняем местами
-            int i = low;
-            int j = high;
-
-            while (i <= j) {
-
+                  quickSort(arr, beginIndex, partitionIndex-1);
+                  quickSort(arr, partitionIndex+1, endIndex);
             }
+      }
+
+      private static int partition(int arr[], int beginIndex, int endIndex) {
+            int pivot = arr[endIndex];
+            int i = (beginIndex-1);
+
+            for (int j = beginIndex; j < endIndex; j++) {
+                  if (arr[j] <= pivot) {
+                        i++;
+
+                        int swapTemp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = swapTemp;
+                  }
+            }
+
+            int swapTemp = arr[i+1];
+            arr[i+1] = arr[endIndex];
+            arr[endIndex] = swapTemp;
+
+            return i+1;
       }
 }
