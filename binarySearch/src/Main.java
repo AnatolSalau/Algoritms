9952ea2988da0cc3a685f684f4b26abc8f6a43ad
AnatolSalau <salovanatoly5@mail.ru>
2023-10-23 13:40:38 +0300

    binarySearch iterable

--- binarySearch/src/Main.java
+++ binarySearch/src/Main.java
@@ -5,13 +5,15 @@ public class Main {
             int[] array = new int[] {10,9,8,7,6,5,4,3,2,1};
             //We can use Binary search only in sorted arrays
             doSort(array);
-            int elementToFind = 5;
-            int index = binarySearchRecursive(array, 0, array.length - 1, elementToFind);
+            int elementToFind = 3;
+            //int indexRecursion = binarySearchRecursive(array, 0, array.length - 1, elementToFind);
+            int indexIterable = binarySearchIterable(array, elementToFind);
             System.out.println("arr : " + Arrays.toString(array));
-            System.out.println("Index number "+ elementToFind +" is " + index );
+            //System.out.println("Index number (recursion search) "+ elementToFind +" is " + indexRecursion );
+            System.out.println("Index number (iterable search) "+ elementToFind +" is " + indexIterable );
 
       }
-      //find index of elements
+      //find index of elements by recursion
       public static int binarySearchRecursive(int[] arr, int startIndex, int endIndex, int elementToFind) {
             //Exit from recursion
             if (endIndex >= startIndex) {
@@ -33,6 +35,28 @@ public class Main {
             return - 1;
       }
 
+      //find index of elements by iteration
+      public static int binarySearchIterable(int[] arr, int elementToFind) {
+            int startIndex = 0;
+            int endIndex = arr.length - 1;
+            int middleIndex;
+            while (startIndex <= endIndex) {
+                  middleIndex = startIndex + (endIndex - startIndex) / 2;
+                  System.out.println("startIndex: " + startIndex + ", endIndex: " + endIndex + ", middleIndex: " + middleIndex);
+
+                  if (arr[middleIndex] == elementToFind) {
+                        System.out.println("found " + elementToFind + " at " + middleIndex);
+                        return middleIndex;
+                  }
+
+                  if (arr[middleIndex] > elementToFind) {
+                        endIndex = middleIndex - 1;
+                  } else {
+                        startIndex = middleIndex + 1;
+                  }
+            }
+            return -1;
+      }
 
       private static void doSort(int[] arr) {
             for (int i = 0; i < arr.length-1; i++){
