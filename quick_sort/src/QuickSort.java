6e6b266e85592e675dd54a1ffca5e23d92751229
AnatolSalau <salovanatoly5@mail.ru>
2023-10-31 16:18:17 +0300

    Is isomorphic strings

--- quick_sort/src/QuickSort.java
+++ quick_sort/src/QuickSort.java
@@ -1,47 +1,44 @@
 import java.util.Arrays;
-
+/**
+ * O n*lon(n)
+ */
 public class QuickSort {
       public static void main(String[] args) {
-            int[] arr = {4, 5, 7, 1, 8, 9};
-            quickSort(arr, 0, arr.length - 1);
-            System.out.println(Arrays.toString(arr));
-            // 1 - 1 4 3 2 5
-            // 5 - 1 4 3 2 5
-            // 2 - 1 2 3 4 5
+            int[] array = {8, 2, 5, 3, 9, 4, 7, 6, 1};
+
+            System.out.println("After before : " + Arrays.toString(array));
+
+            quickSort(array, 0, array.length - 1);
+
+            System.out.println("After after : " + Arrays.toString(array));
       }
 
-      private static void quickSort(int[] arr, int leftIndex, int rightIndex) {
-            //exit from recursion
-            if(leftIndex >= rightIndex) return;
+      private static void quickSort(int[] array, int startIndex, int endIndex) {
+            if(endIndex<= startIndex) return; //base case
 
-            int pivotIndex = partition(arr, leftIndex, rightIndex);
-            //run quickSort for left part of arr
-            quickSort(arr, leftIndex, pivotIndex - 1);
+            int pivotIndex = partition(array,startIndex, endIndex);
 
-            //run quickSort for right part of arr
-            quickSort(arr, pivotIndex+1, rightIndex);
+            quickSort(array,startIndex,pivotIndex - 1);
+            quickSort(array,pivotIndex +1,endIndex);
       }
 
-      private static int partition(int[] arr, int leftIndex, int rightIndex) {
-           int pivot = arr[rightIndex];
-           int pointerIndex = leftIndex - 1;
+      private static int partition(int[] array, int startIndex, int endIndex) {
+            int pivot = array[endIndex];
+            int i = startIndex - 1;
 
-            for (int i = leftIndex; i < rightIndex; i++) {
-                  if(arr[i] < pivot) {
-                       pointerIndex++;
-                       swap(arr, pointerIndex, i);
+            for (int j = startIndex; j <= endIndex - 1; j++) {
+                  if(array[j] < pivot) {
+                        i++;
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
                   }
             }
-            //swap last element in arr
-            swap(arr, pointerIndex + 1, rightIndex);
-            int sout = arr[pointerIndex + 1];
-            System.out.println("Return" + sout );
-            return pointerIndex + 1;
-      }
+            i++;
+            int temp = array[i];
+            array[i] = array[endIndex];
+            array[endIndex] = temp;
 
-      private static void swap(int[] arr, int pointerIndex, int i) {
-            int temp = arr[i];
-            arr[i] = arr[pointerIndex];
-            arr[pointerIndex] = temp;
+            return i;
       }
 }
---
+++ quick_sort/src/QuickSortTest.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+
+public class QuickSortTest {
+      public static void main(String[] args) {
+            int[] array = {1,9, 5, 2, 8, -1, -9 };
+            System.out.println("After before : " + Arrays.toString(array));
+
+
+
+            System.out.println("After after : " + Arrays.toString(array));
+      }
+}
