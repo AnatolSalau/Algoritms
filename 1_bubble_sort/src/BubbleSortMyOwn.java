51226d202fbcf4f51f4730bbd0f5866159c1d972
AnatolSalau <salovanatoly5@mail.ru>
2023-11-01 14:41:11 +0300

    remove duplicates

--- 1_bubble_sort/src/BubbleSortMyOwn.java
+++ 1_bubble_sort/src/BubbleSortMyOwn.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 /**
  * Bubble sort O(N) worst o(N*N)
  */
-public class BubbleSort {
+public class BubbleSortMyOwn {
       public static void main(String[] args) {
             int[] arr = {10, 4, 1, 5, 2, -1, -1, 3, 10,};
             //1.1 (4 5 2 3) -> (4 5 2 3)
@@ -22,23 +22,12 @@ public class BubbleSort {
       */
 
             System.out.println("arr after : " + Arrays.toString(arr));
-            doSort(arr);
+            doSortMyOwn(arr);
             System.out.println("arr before : " + Arrays.toString(arr));
       }
 
-      private static void doSort(int[] arr) {
-            for (int i = 0; i < arr.length - 1; i++){
-                  for (int j = i+1; j < arr.length; j++){
-                        if (arr[ i] > arr[ j]) {
-                              int t = arr[ i];
-                              arr[ i] = arr[ j];
-                              arr[ j] = t;
-                        }
-                  }
-            }
-      }
-
       private static void doSortMyOwn(int[] arr) {
+
 
       }
 }
--- quick_sort/src/QuickSortTest.java
+++ quick_sort/src/QuickSortTest.java
@@ -6,8 +6,6 @@ public class QuickSortTest {
             int[] array = {1,9, 5, 2, 8, -1, -9 };
             System.out.println("After before : " + Arrays.toString(array));
 
-
             System.out.println("After after : " + Arrays.toString(array));
       }
-
 }
