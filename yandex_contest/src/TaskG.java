70c50a9bb739af70b2fbe3f263bf5f22db98587b
AnatolSalau <salovanatoly5@mail.ru>
2023-11-04 23:48:20 +0300

    Yandex contest Task D

--- yandex_contest/src/TaskG.java
+++ yandex_contest/src/TaskG.java
@@ -53,7 +53,7 @@ public class TaskG {
 
       static void printMatrix(int[][] matrix) {
             for (int i = 0; i < matrix.length; i++) {
-                  System.out.println(Arrays.toString(matrix[i]));
+                  System.out.println("City N" + i +" : " +  Arrays.toString(matrix[i]));
             }
       }
 }
