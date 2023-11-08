0b2fabd5f2088fca77963005485cb67a155cecdf
AnatolSalau <salovanatoly5@mail.ru>
2023-11-08 16:17:06 +0300

    SubarraySumEqualsK

---
+++ search_in_string/src/PowerSearch.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class PowerSearch {
+      public static void main(String[] args) {
+            String text =   "aabaabaaaaaabaabaabaabbaaab";
+            String sample = "aabaab";
+
+            System.out.println(Arrays.toString(poverSearch(text, sample)));
+      }
+
+      public static int[] poverSearch(String text, String sample) {
+
+      }
+}
