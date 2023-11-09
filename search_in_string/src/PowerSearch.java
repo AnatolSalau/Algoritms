571b938b1e72850cdbdf838c81369a89afb89cc2
AnatolSalau <salovanatoly5@mail.ru>
2023-11-09 12:19:17 +0300

    Power search in string

--- search_in_string/src/PowerSearch.java
+++ search_in_string/src/PowerSearch.java
@@ -1,14 +1,45 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PowerSearch {
+      /**
+       * return start indexes of sample's enters
+       */
       public static void main(String[] args) {
             String text =   "aabaabaaaaaabaabaabaabbaaab";
             String sample = "aabaab";
 
-            System.out.println(Arrays.toString(poverSearch(text, sample)));
+            System.out.println(powerSearch(text, sample));
       }
+      /*    i = 0
+            aabaabaaaaaabaabaabaabbaaab
+            aabaab
+                 i+j= 0 + 5 -> result.add(0)
 
-      public static int[] poverSearch(String text, String sample) {
+                      i = 10
+            aabaabaaaaaabaabaabaabbaaab
+                      aabaab
+                           i+j = 10 + 5 -> result.add(10)
 
+           [0, 10, 13, 16]
+       */
+      public static List<Integer> powerSearch(String text, String sample) {
+            char[] textChars = text.toCharArray();
+            char[] sampleChars = sample.toCharArray();
+           List<Integer> result = new ArrayList<>();
+            for (int i = 0; i < textChars.length; i++) {
+                  for (int j = 0; j < sampleChars.length; j++) {
+                        //check we went beyond the array boundary or not
+                        if (j + i >= textChars.length) return result;
+
+                        if (textChars[j+i] != sampleChars[j]) {
+                              break;
+                        }
+                        if (j == sampleChars.length - 1) {
+                              result.add(i);
+                        }
+                  }
+            }
+            return result;
       }
 }
