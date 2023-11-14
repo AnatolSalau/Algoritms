0cd07cea6ecb2690b0483c88460ff3186fa52b4a
AnatolSalau <salovanatoly5@mail.ru>
2023-11-14 12:35:50 +0300

    MergeIntervals  - Quick sort

--- MustHave/Sorts/src/MergeIntervals.java
+++ MustHave/Sorts/src/MergeIntervals.java
@@ -30,12 +30,13 @@ public class MergeIntervals {
       }
 
       static void test3() {
-            int[][] arrays = {{1,3},{2,6},{8,10},{8,9},{9,11},{15,18},{2,4},{16, 17}};
+            int[][] arrays = {{2,6},{8,10},{8,9},{9,11},{15,18},{2,4},{16, 17},{1,3}};
             int[][] expectedResult = {{1,6}, {8,11}, {15,18}};
             MergeIntervals mergeIntervals = new MergeIntervals();
             System.out.println("Interfals before sort : " + Arrays.deepToString(arrays));
-            mergeIntervals.sortIntervalsBubble(arrays);
+            mergeIntervals.sortIntervalsQuick(arrays,0,arrays.length - 1);
             System.out.println("Interfals after sort : " + Arrays.deepToString(arrays));
+
       }
 
       private void sortIntervalsBubble(int[][] intervals) {
@@ -54,7 +55,41 @@ public class MergeIntervals {
             }
       }
 
-      private void sortIntervalsQuick(int[] array) {
+      private void sortIntervalsQuick(int[][] array, int leftInd, int rightInd){
+            if (rightInd <= leftInd) return;
 
+            int pivotInd = particion(array,leftInd, rightInd);
+            sortIntervalsQuick(array,leftInd,pivotInd - 1);
+            sortIntervalsQuick(array, pivotInd +1, rightInd);
+      }
+
+      private int particion(int[][] array, int leftInd, int rightInd) {
+            int pivot = array[rightInd][0];
+            int i = leftInd - 1;
+
+            for (int j = leftInd; j <=rightInd - 1 ; j++) {
+                  if (array[j][0] < pivot) {
+                        i++;
+                        int[] temp = new int[2];
+
+                        temp[0] = array[j][0];
+                        temp[1] = array[j][1];
+
+                        array[j][0] = array[i][0];
+                        array[j][1] = array[i][1];
+
+                        array[i][0] = temp[0];
+                        array[i][1] = temp[1];
+                  }
+            }
+            i++;
+            int[] temp = new int[2];
+            temp[0] = array[rightInd][0];
+            temp[1] = array[rightInd][1];
+            array[rightInd][0] = array[i][0];
+            array[rightInd][1] = array[i][1];
+            array[i][0] = temp[0];
+            array[i][1] = temp[1];
+            return i;
       }
 }
