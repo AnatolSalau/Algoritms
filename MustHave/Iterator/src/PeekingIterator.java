add4a661f8906f9bd938eb935695b2c24939667c
AnatolSalau <salovanatoly5@mail.ru>
2023-11-17 14:16:51 +0300

    LongestCharacterRepeating

---
+++ MustHave/Iterator/src/PeekingIterator.java
@@ -0,0 +1,43 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+import java.util.Iterator;
+
+class PeekingIterator implements Iterator<Integer> {
+      private Iterator<Integer> iterator = null;
+      private Integer next = null;
+      public PeekingIterator(Iterator<Integer> iterator) {
+            // initialize any member here.
+            this.iterator = iterator;
+            if(iterator.hasNext()) {
+                  this.next = this.iterator.next();
+            }
+
+      }
+
+      // Returns the next element in the iteration without advancing the iterator.
+      public Integer peek() {
+            return next;
+      }
+
+      // hasNext() and next() should behave the same as in the Iterator interface.
+      // Override them if needed.
+      @Override
+      public Integer next() {
+            Integer returnValue = next;
+            if(iterator.hasNext()) {
+                  this.next = iterator.next();
+            } else {
+                  this.next = null;
+            }
+            return returnValue;
+      }
+
+      @Override
+      public boolean hasNext() {
+            if(next != null) {
+                  return true;
+            }
+            return false;
+      }
+}
