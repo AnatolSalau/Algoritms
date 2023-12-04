96b5c3f156801c2bc3805261f2d75d2cd12edf35
AnatolSalau <salovanatoly5@mail.ru>
2023-12-04 16:00:03 +0300

    Task34 SquaresOfSortedArray

--- YaroslayTasks/src/task34/Task34.java
+++ YaroslayTasks/src/task34/Task34.java
@@ -18,13 +18,16 @@ public class Task34 {
        * Output: [4,9,9,49,121]
        */
       public static void main(String[] args) {
+            System.out.println("Task34");
             int[] nums1 = {-4, -1, 0, 3, 10};
+            System.out.println(Arrays.toString(nums1));
             System.out.println("Right answer : ");
             System.out.println("[0, 1, 9, 16, 100]");
             int[] res1 = sortedSquares(nums1);
             System.out.println(Arrays.toString(res1));
             System.out.println();
             int[] nums2 = {-3,2,3,11};
+            System.out.println(Arrays.toString(nums2));
             System.out.println("Right answer : ");
             System.out.println("[4,9,9,121]");
             int[] res2 = sortedSquares(nums2);
